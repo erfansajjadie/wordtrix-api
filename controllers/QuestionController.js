649b4fb01900c4d1b5846625723bfce33b737098
erfansajjadie <erfan30101377@gmail.com>
2022-08-25 12:37:57 +0430

    added question create api

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -8,6 +8,7 @@ class QuestionController {
 
     static async createQuestion (req, res) {
         let data = req.body;
+        this.createQuestionValidations();
         return res.status(201).send({message: "Question Created", question: data})
         const question = await Question.create(data)
         return res.status(201).send({message: "Question Created", question: question})
